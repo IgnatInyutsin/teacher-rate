2d16be1c5393be3980a96bb4a058c9d9c50f5f54
IgnatInyutsin <IgnatInyutsin>
2023-04-10 18:26:03 +0400

    initial commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+import {Routes, RouterModule} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule }   from '@angular/common/http';
+import {CookieService} from "ngx-cookie-service";
+
+// определение маршрутов
+const appRoutes: Routes = [];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+  ],
+  providers: [
+    CookieService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
