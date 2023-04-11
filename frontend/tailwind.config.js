995fec98239900af955269e849a55bf72934dd55
Lumiere <duracchki88rus@gmail.com>
2023-04-11 08:52:00 +0400

    Добавил 2 кнопки и input в header

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        mainBGColor: '#0F172A'
+      },
+    },
   },
   plugins: [],
 }
