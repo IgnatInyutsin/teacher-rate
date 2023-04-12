5798fa2119fd74d94ab2007bcfbed7596ff965da
Lumiere <duracchki88rus@gmail.com>
2023-04-12 11:23:19 +0400

    Сделал добавление темы в кук

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Connector} from "../restapi";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-header',
@@ -10,18 +13,24 @@ export class HeaderComponent implements OnInit {
   darkTheme:string = "dark";
   onDarkTheme:boolean = true;
 
-  constructor() { }
+  constructor(private http: HttpClient, private connector: Connector, public cookieService: CookieService) { }
 
   ngOnInit(): void {
+    if (this.cookieService.get("theme") == "false") {
+      document.documentElement.classList.remove(this.darkTheme);
+    }
   }
   switched_theme():void{
     if (this.onDarkTheme){
       document.documentElement.classList.remove(this.darkTheme);
       this.onDarkTheme = false
+      this.cookieService.set("theme",String(this.onDarkTheme))
     }
     else {
       document.documentElement.classList.add(this.darkTheme);
       this.onDarkTheme = true
+      this.cookieService.set("theme",String(this.onDarkTheme))
+
     }
   }
 
