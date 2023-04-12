9ac3ffc0a0c8d166b2490d65e6578bed89767539
Leontjev Michael <michlea@yandex.ru>
2023-04-12 15:18:22 +0400

    feat: add teacher retrieve

--- backend/api/restapi/app/serializers.py
+++ backend/api/restapi/app/serializers.py
@@ -16,12 +16,32 @@ class ReviewSerializer(serializers.ModelSerializer):
                        grade_count=teacher[0].grade_count+1)
         return Review.objects.create(**validated_data)
 
+
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = ("name", "city", "job_place", "photo", "grade")
 
+
+class FullTeacherSerializer(serializers.ModelSerializer):
+    class TeacherReviewSerializer(serializers.ModelSerializer):
+        class AuthorSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = User
+                fields = ("username", )
+
+        author = AuthorSerializer()
+        class Meta:
+            model = Review
+            fields = ("author", "title", "main_text", "grade", "teacher")
+
+    reviews = TeacherReviewSerializer(many=True)
+    class Meta:
+        model = Teacher
+        fields = "__all__"
+
+
 class TeacherCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
-        fields = ("name", "description", "study_place", "jobplace", "city", "photo")
+        fields = ("name", "description", "study_place", "job_place", "city", "photo")
